e3bca01f89fc2e5849b9de2eecfea42b2db4abd0
lzwdgc <lzwdgc@gmail.com>
2017-02-20 17:20:49 +0300

    Add project launcher.

---
+++ Polygon4.bat
@@ -0,0 +1,18 @@
+@echo off
+
+setlocal ENABLEEXTENSIONS
+set KEY_NAME=HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\4.15
+set VALUE_NAME=InstalledDirectory
+
+FOR /F "skip=2 tokens=1,2*" %%A IN ('%windir%\Sysnative\REG QUERY "%KEY_NAME%" /v "%VALUE_NAME%" 2^>nul') DO (
+    set ValueName=%%A
+    set ValueType=%%B
+    set ValueValue=%%C
+)
+
+if not defined ValueName (
+    @echo "%KEY_NAME%"\"%VALUE_NAME%" not found.
+)
+
+set exe=%ValueValue%\Engine\Binaries\Win64\UE4Editor.exe
+start "" "%exe%" %cd%\Polygon4.uproject -ansimalloc
