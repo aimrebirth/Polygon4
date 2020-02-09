b47583e153a6b3265bba932462252cbe9ab0d10b
lzwdgc <lzwdgc@gmail.com>
2020-02-10 01:17:52 +0300

    Update p4.bat.

--- Polygon4.bat
+++ Polygon4.bat
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal ENABLEEXTENSIONS
-set KEY_NAME=HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\4.22
+set KEY_NAME=HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\4.24
 set VALUE_NAME=InstalledDirectory
 
 FOR /F "skip=2 tokens=1,2*" %%A IN ('%windir%\Sysnative\REG QUERY "%KEY_NAME%" /v "%VALUE_NAME%" 2^>nul') DO (
@@ -30,5 +30,10 @@ if not defined ValueName (
 	)
 )
 
+:: mv old dbghelp, system uses newer
+move %ValueValue%\Engine\Binaries\ThirdParty\DbgHelp\dbghelp.dll %ValueValue%\Engine\Binaries\ThirdParty\DbgHelp\dbghelp_old.dll
+
 set exe=%ValueValue%\Engine\Binaries\Win64\UE4Editor.exe
-start "" "%exe%" %~dp0\Polygon4.uproject -ansimalloc %*
+::start "" "%exe%" %~dp0\Polygon4.uproject -ansimalloc %*
+:: -ansimalloc not needed today
+start "" "%exe%" %~dp0\Polygon4.uproject %*
